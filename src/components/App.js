dfebc8fa604a6ac30c326b1e8ac185d620f4d015
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-06-20 17:06:37 -0400

    able t creat user and sign in userr

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "../styles/App.css";
 import Home from "../pages/Home";
+import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
 import Favorites from "../pages/Favorites";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Signup />
       <Routes>
         <Route path="/search" element={<Search />} />
         <Route path="/" element={<Home />} />
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,10 +1,60 @@
-import React from "react";
-import "../styles/Signin.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Signin() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://project-3-manga-backend-2d7dcb1090ee.herokuapp.com/users/signin",
+        {
+          name,
+          password,
+        }
+      );
+      if (response.status === 200) {
+        setName("");
+        setPassword("");
+        console.log(`Success signed in ${name}`);
+      } else {
+        console.log("Error:", response.status);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Signin!</h1>
+      <h1>Sign In!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={name} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
     </div>
   );
 }

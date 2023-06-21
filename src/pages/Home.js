7b4bf65c3d7aca26b47cfd2daafcc70271a6386c
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-06-21 09:39:52 -0400

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,42 @@
-import React from "react";
+// pages/Home.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Home() {
+const Home = () => {
+  const [mangas, setMangas] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMangas = async () => {
+      try {
+        const response = await axios.get(
+          "https://project-3-manga-backend-2d7dcb1090ee.herokuapp.com/mangas/all"
+        );
+        console.log(response);
+        setMangas(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchMangas();
+  }, []);
+
   return (
-    <div>
-      <h1>Home!</h1>
+    <div className="manga-grid">
+      {mangas.length > 0 ? (
+        mangas.map((manga) => (
+          <div key={manga._id} className="manga-card">
+            <img src={manga.picture_url} alt={manga.title} />
+            <h3>{manga.title}</h3>
+          </div>
+        ))
+      ) : (
+        <p>No mangas found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default Home;

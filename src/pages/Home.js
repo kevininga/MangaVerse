0ac48553da93b1577805b9f1ff03a0bd6d746a96
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-06-21 10:25:43 -0400

    added auth header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
 // pages/Home.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../styles/Home.css";
 
 const Home = () => {
   const [mangas, setMangas] = useState([]);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchMangas = async () => {
       const response = await axios.get(
-        "https://project-3-manga-backend-2d7dcb1090ee.herokuapp.com/mangas/all"
+        "https://project-3-manga-backend-2d7dcb1090ee.herokuapp.com/mangas/all",{
+
+        headers: {
+          Authorization: "Bearer a4b2bd6771msh2c09a033218be90p17b986jsna07b85b48652"
+        }
+          
+        }
       );
+      console.log(response)
       setMangas(response.data);
+      console.log(response.data);
     };
 
     fetchMangas();
@@ -19,17 +27,12 @@ const Home = () => {
 
   return (
     <div className="manga-grid">
-      {mangas.length > 0 ? (
-        mangas.map((manga) => (
-          <div key={manga._id} className="manga-card">
-            <img src={manga.picture_url} alt={manga.title} />
-            <h3>{manga.title}</h3>
-          </div>
-        ))
-      ) : (
-        <p>No mangas found.</p>
-      )}
-
+      {mangas.map((manga) => (
+        <div key={manga._id} className="manga-card">
+          <img src={manga.picture_url} alt={manga.title} />
+          <h3>{manga.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
